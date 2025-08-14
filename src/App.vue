79f207b6e9c40a69dc9423dcb82cfda780c390c8
Anthony Stackman <Anth.stackman@hotmail.co.uk>
2025-08-14 12:47:30 +0100

    Routing

--- src/App.vue
+++ src/App.vue
@@ -3,70 +3,86 @@
     <nav>
       <ul>
         <li class="nav-item">
-          <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />
-          Build-a-Bot
+          <router-link :to="{ name: 'Home' }" class="nav-link" active-class="home-active">
+            <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />
+            Build-a-Bot
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Build' }" class="nav-link">
+            Build
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Search' }" class="nav-link">
+            Search
+          </router-link>
         </li>
       </ul>
     </nav>
   </header>
   <main>
-      <RobotBuilder />
-    <!--<HomePage msg="Welcome to Your Vue.js App" />-->
+    <router-view />
   </main>
 </template>
 
-<script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
-
-export default {
-  name: 'App',
-  components: {
-    RobotBuilder,
-  },
-};
+<script setup>
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
 
-body {
-  background: linear-gradient(to bottom, #555, #999);
-  background-attachment: fixed;
-}
+  body {
+    background: linear-gradient(to bottom, #555, #999);
+    background-attachment: fixed;
+  }
 </style>
 
 <style scoped>
-main {
-  margin: 0 auto;
-  padding: 30px;
-  background: #fff;
-  width: 1400px;
-  min-height: 300px;
-}
+  main {
+    margin: 0 auto;
+    padding: 30px;
+    background: #fff;
+    width: 1400px;
+    min-height: 300px;
+  }
 
-header {
-  background-color: #999;
-  width: 1460px;
-  margin: 0 auto
-}
+  header {
+    background-color: #999;
+    width: 1460px;
+    margin: 0 auto
+  }
 
-ul {
-  padding: 3px;
-  display: flex;
-}
+  ul {
+    padding: 3px;
+    display: flex;
+  }
 
-.nav-item {
-  display: inline-block;
-  padding: 5px 10px;
-  font-size: 22px;
-  border-right: 1px solid #bbb;
-}
+  .nav-item {
+    display: inline-block;
+    padding: 5px 10px;
+    font-size: 22px;
+    border-right: 1px solid #bbb;
+  }
 
-.logo {
-  vertical-align: middle;
-  height: 30px;
-}
+  .nav-link {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  .logo {
+    vertical-align: middle;
+    height: 30px;
+  }
+
+  .active-link {
+    color: #fff;
+  }
+
+  .home-active {
+    color: #fff;
+    text-shadow: 2px 2px black;
+  }
 </style>
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,19 +1,23 @@
 <template>
     <div class="content">
         <div class="preview">
-            <div class="preview-content">
-                <div class="top-row">
-                    <img :src="selectedRobot.head.imageUrl" alt="" />
+            <CollapsibleSection>
+                <template v-slot:collapse>&#x25B2; Hide</template>
+                <template v-slot:expand>&#x25BC; Show</template>
+                <div class="preview-content">
+                    <div class="top-row">
+                        <img :src="selectedRobot.head.imageUrl" alt="" />
+                    </div>
+                    <div class="middle-row">
+                        <img :src="selectedRobot.leftArm.imageUrl" class="rotate-left" alt="" />
+                        <img :src="selectedRobot.torso.imageUrl" alt="" />
+                        <img :src="selectedRobot.rightArm.imageUrl" class="rotate-right" alt="" />
+                    </div>
+                    <div class="bottom-row">
+                        <img :src="selectedRobot.base.imageUrl" alt="" />
+                    </div>
                 </div>
-                <div class="middle-row">
-                    <img :src="selectedRobot.leftArm.imageUrl" class="rotate-left" alt="" />
-                    <img :src="selectedRobot.torso.imageUrl" alt="" />
-                    <img :src="selectedRobot.rightArm.imageUrl" class="rotate-right" alt="" />
-                </div>
-                <div class="bottom-row">
-                    <img :src="selectedRobot.base.imageUrl" alt="" />
-                </div>
-            </div>
+            </CollapsibleSection>
             <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
         </div>
         <div class="top-row">
@@ -54,150 +58,151 @@
 </template>
 
 <script setup>
-import { computed, ref, onMounted } from 'vue';
-import { toCurrency } from '@/shared/formatters';
-import PartSelector from './PartSelector.vue';
-import parts from '../data/parts';
+    import { computed, ref, onMounted } from 'vue';
+    import { toCurrency } from '@/shared/formatters';
+    import CollapsibleSection from '@/shared/CollapsibleSection.vue';
+    import PartSelector from './PartSelector.vue';
+    import parts from '../data/parts';
 
-const availableParts = parts;
-const cart = ref([]);
+    const availableParts = parts;
+    const cart = ref([]);
 
-onMounted(() => console.log('On Mounted'));
+    onMounted(() => console.log('On Mounted'));
 
-const selectedRobot = ref({
-  head: {},
-  leftArm: {},
-  torso: {},
-  rightArm: {},
-  base: {},
-});
+    const selectedRobot = ref({
+        head: {},
+        leftArm: {},
+        torso: {},
+        rightArm: {},
+        base: {},
+    });
 
-const headBorderColor = computed(() => (selectedRobot.value.head.onSale ? 'red' : '#aaa'));
+    const headBorderColor = computed(() => (selectedRobot.value.head.onSale ? 'red' : '#aaa'));
 
-const addToCart = () => {
-  const robot = selectedRobot.value;
-  const cost = robot.head.cost +
-        robot.leftArm.cost +
-        robot.torso.cost +
-        robot.rightArm.cost +
-        robot.base.cost;
-  cart.value.push({ ...robot, cost });
-  console.log(cart.value.length);
-};
+    const addToCart = () => {
+        const robot = selectedRobot.value;
+        const cost = robot.head.cost +
+            robot.leftArm.cost +
+            robot.torso.cost +
+            robot.rightArm.cost +
+            robot.base.cost;
+        cart.value.push({ ...robot, cost });
+        console.log(cart.value.length);
+    };
 </script>
 
 <style lang="scss" scoped>
-.part {
-    position: relative;
-    width: 200px;
-    height: 200px;
-    border: 3px solid #aaa;
-
-    &.top {
-        border: 3px solid v-bind(headBorderColor);
-    }
-
-    img {
+    .part {
+        position: relative;
         width: 200px;
+        height: 200px;
+        border: 3px solid #aaa;
+
+        &.top {
+            border: 3px solid v-bind(headBorderColor);
+        }
+
+        img {
+            width: 200px;
+        }
     }
-}
 
-.top-row {
-    display: flex;
-    justify-content: space-around;
-}
-
-.middle-row {
-    display: flex;
-    justify-content: center;
-}
-
-.bottom-row {
-    display: flex;
-    justify-content: space-around;
-    border-top: none;
-}
-
-.top {
-    border-bottom: none;
-}
-
-.left {
-    border-right: none;
-
-    img {
-        transform: rotate(-90deg);
+    .top-row {
+        display: flex;
+        justify-content: space-around;
     }
-}
 
-.right {
-    border-left: none;
+    .middle-row {
+        display: flex;
+        justify-content: center;
+    }
 
-    img {
+    .bottom-row {
+        display: flex;
+        justify-content: space-around;
+        border-top: none;
+    }
+
+    .top {
+        border-bottom: none;
+    }
+
+    .left {
+        border-right: none;
+
+        img {
+            transform: rotate(-90deg);
+        }
+    }
+
+    .right {
+        border-left: none;
+
+        img {
+            transform: rotate(90deg);
+        }
+    }
+
+    .bottom {
+        border-top: none;
+    }
+
+    .robot-name {
+        position: absolute;
+        top: -25px;
+        text-align: center;
+        width: 100%;
+    }
+
+    .sale {
+        color: red;
+    }
+
+    .content {
+        position: relative;
+    }
+
+    .preview {
+        position: absolute;
+        top: -20px;
+        right: 0;
+        width: 310px;
+        height: 310px;
+        padding: 5px;
+    }
+
+    .preview-content {
+        border: 1px solid #999;
+    }
+
+    .preview img {
+        width: 70px;
+        height: 70px;
+    }
+
+    .rotate-right {
         transform: rotate(90deg);
     }
-}
 
-.bottom {
-    border-top: none;
-}
+    .rotate-left {
+        transform: rotate(-90deg);
+    }
 
-.robot-name {
-    position: absolute;
-    top: -25px;
-    text-align: center;
-    width: 100%;
-}
+    .add-to-cart {
+        position: absolute;
+        width: 310px;
+        padding: 3px;
+        font-size: 16px;
+    }
 
-.sale {
-    color: red;
-}
+    td,
+    th {
+        text-align: left;
+        padding: 5px;
+        padding-right: 20px;
+    }
 
-.content {
-    position: relative;
-}
-
-.preview {
-  position: absolute;
-  top: -20px;
-  right: 0;
-  width: 310px;
-  height: 310px;
-  padding: 5px;
-}
-
-.preview-content {
-  border: 1px solid #999;
-}
-
-.preview img {
-  width: 70px;
-  height: 70px;
-}
-
-.rotate-right {
-  transform: rotate(90deg);
-}
-
-.rotate-left {
-  transform: rotate(-90deg);
-}
-
-.add-to-cart {
-    position: absolute;
-    width: 310px;
-    padding: 3px;
-    font-size: 16px;
-}
-
-td,
-th {
-    text-align: left;
-    padding: 5px;
-    padding-right: 20px;
-}
-
-.cost {
-    text-align: right;
-}
+    .cost {
+        text-align: right;
+    }
 </style>
